73acaff7aa48f9b4a36b2a5f320e8ecf6e7fcf10
gAldeia <guilherme.seidyo@gmail.com>
2023-07-02 13:34:27 -0400

    new attempt

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -132,7 +132,7 @@ def test_linear_ITExpr_equals_scikit_logisticRegression(
         solver='saga',
         max_iter=100,
         multi_class='multinomial',
-        penalty=None,
+        penalty='none',
         random_state=42
     ).fit(X, y)
 
