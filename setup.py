110f8ffcf1f28909109c2131fe2d8a87e8a3ab21
gAldeia <guilherme.seidyo@gmail.com>
2023-07-02 10:43:28 -0400

    Starting to separate install and test/doc requirements
    The idea is that `setup.py` will specify only the minimal requirements.
    There is no need to have the documentation requirements there, but
    we need to make sure it is on requirement file in order to allow
    github to build the docs on commit

--- setup.py
+++ setup.py
@@ -12,40 +12,45 @@ setup(
     long_description = description[:description.find('-----')],
     long_description_content_type = 'text/markdown',
     author           = "Guilherme Aldeia",
-    author_email="guilherme.aldeia@ufabc.edu.br",
+    author_email     = "guilherme.aldeia@ufabc.edu.br",
     license          = "BSD-3-Clause",
     install_requires = [
+        # ITEA core
         "numpy>=1.18.2",
         "scikit-learn>=0.23.1",
-        "matplotlib>=3.2.2",
-        "pandas>=1.1.0",
         "jax>=0.2.13",
         "jaxlib>=0.1.67",
         "scipy>=1.5.2",
-        "pylatex==1.4.1",
-        "docutils==0.17.1"
+
+        # ITEA summarizer
+        "matplotlib>=3.2.2",
+        "pandas>=1.1.0",
+        "pylatex>=1.4.1"
     ],
     package_data = {'examples' : ['examples']},
     include_package_data = True,
     python_requires = '>=3.7',
     setup_requires  = [
+        "setuptools>=42",
         "wheel",
-        "pytest-runner",
-        "coverage",
-        "coverage-badge",
 
-        #required to build the docs
-        "sphinx",
-        "Jinja2==2.11",  
-        "markupsafe==2.0.1",
-        "sphinx_rtd_theme==1.0.0",
-        "ipython==8.5.0",
-        "nbsphinx"
+        # #required to build the docs
+        # "sphinx",
+        # "Jinja2==2.11",  
+        # "markupsafe==2.0.1",
+        # "sphinx_rtd_theme==1.0.0",
+        # "ipython==8.5.0",
+        # "nbsphinx",
+        # "docutils==0.17.1"
     ],
 
     # Aditional packages not included in install requires
     tests_require   = [
-        # Used in tests
+        # Testing
+        # "pytest-runner",
+        # "coverage",
+        # "coverage-badge",
+
         "pytest",
         "pytest-html",
 
